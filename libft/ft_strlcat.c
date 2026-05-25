71dda1d00ee8f06b86b6e4b89475382dad57aa3d
Alessandro Belli <abelli@c2r4s5.42roma.it>
2026-05-25 16:46:50 +0200

    pro

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelli <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 16:23:48 by abelli            #+#    #+#             */
+/*   Updated: 2026/05/21 16:23:53 by abelli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	d_len;
+	size_t	s_len;
+	size_t	i;
+
+	d_len = 0;
+	s_len = 0;
+	while (dst[d_len] && d_len < size)
+		d_len++;
+	while (src[s_len])
+		s_len++;
+	if (d_len == size)
+		return (size + s_len);
+	i = 0;
+	while (src[i] && (d_len + i < size - 1))
+	{
+		dst[d_len + i] = src[i];
+		i++;
+	}
+	dst[d_len + i] = '\0';
+	return (d_len + s_len);
+}
